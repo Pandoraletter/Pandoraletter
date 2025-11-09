6275b58a069aa87fecde5224fef2eabde423b483
Pandoraletter <felipeortz223@gmail.com>
2025-11-09 17:54:43 -0500

    Add files via upload

--- temporizador.js
+++ temporizador.js
@@ -1,5 +1,5 @@
 // Fecha final fija (ejemplo: 31 de agosto de 2025 a las 18:00)
-const endDate = new Date("2025-11-01T10:00:00").getTime();
+const endDate = new Date("2025-12-01T10:00:00").getTime();
 
 const reloj = document.querySelector(".reloj");
 const botonCofre = document.getElementById("abrirCofre");
